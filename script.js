faf821ad417d73bbd74a69dcc5f3e7ae4a3adb0a
Nihita Lele <124602601+NihitaLele@users.noreply.github.com>
2024-02-28 18:00:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 function fibonacci(num) {
-// your code here
+    if (num <= 0) {
+        return 0; 
+    } else if (num === 1) {
+        return 1; 
+    } else {
+        let fib = []; 
+        fib[0] = 0;
+        fib[1] = 1;
+
+        for (let i = 2; i <= num; i++) {
+            fib[i] = fib[i - 1] + fib[i - 2];
+        }
+
+        return fib[num];
+    }
 }
 
 module.exports = fibonacci;
